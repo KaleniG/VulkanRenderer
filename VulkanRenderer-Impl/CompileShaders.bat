c6f9d20229e385d6f9b1b78ce3e910dbc28c825b
KaleniG <kalenig.01@gmail.com>
2024-07-18 22:03:06 +0200

    Updated CompileShaders.bat
    - Now it updates all the .vert and .frag shaders

--- VulkanRenderer-Impl/CompileShaders.bat
+++ VulkanRenderer-Impl/CompileShaders.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/Shader.vert -o assets/shaders/Shader.vert.spv
-CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/Shader.frag -o assets/shaders/Shader.frag.spv
+FOR %%I IN (Assets/Shaders/*.frag) DO CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/%%I -o Assets/Shaders/%%I.spv
+FOR %%I IN (Assets/Shaders/*.vert) DO CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/%%I -o Assets/Shaders/%%I.spv
 PAUSE
