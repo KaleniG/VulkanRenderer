a2409970d4abc4ab6e24621693831383507ab280
KaleniG <kalenig.01@gmail.com>
2024-09-28 21:16:17 +0200

    Now compiling all kind of shader

--- VulkanRenderer-Impl/CompileShaders.bat
+++ VulkanRenderer-Impl/CompileShaders.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
 FOR %%I IN (Assets/Shaders/*.frag) DO CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/%%I -o Assets/Shaders/%%I.spv
 FOR %%I IN (Assets/Shaders/*.vert) DO CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/%%I -o Assets/Shaders/%%I.spv
+FOR %%I IN (Assets/Shaders/*.tesc) DO CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/%%I -o Assets/Shaders/%%I.spv
+FOR %%I IN (Assets/Shaders/*.tese) DO CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/%%I -o Assets/Shaders/%%I.spv
+FOR %%I IN (Assets/Shaders/*.geom) DO CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/%%I -o Assets/Shaders/%%I.spv
+FOR %%I IN (Assets/Shaders/*.comp) DO CALL %%VULKAN_SDK%%/Bin/glslc.exe Assets/Shaders/%%I -o Assets/Shaders/%%I.spv
 PAUSE
