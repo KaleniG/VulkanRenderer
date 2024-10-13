a0be85fea8d06ae103c14d49d4bfa38b5f3d5cae
KaleniG <kalenig.01@gmail.com>
2024-10-13 12:20:15 +0200

    Platform-specific 'EntryPoint'
    - Now no console window when building in 'Distribution' setting

--- VulkanRenderer-Core/Source/VulkanRenderer/Core/EntryPoint.h
+++ VulkanRenderer-Core/Source/VulkanRenderer/Core/EntryPoint.h
@@ -5,6 +5,20 @@
 
 extern vkren::Application* vkren::CreateApplication();
 
+#ifdef PLATFORM_WINDOWS
+
+#include <windows.h>
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+  vkren::Log::Init();
+  vkren::Application* app = vkren::CreateApplication();
+  app->Run();
+  delete app;
+}
+
+#else
+ 
 int main()
 {
   vkren::Log::Init();
@@ -12,3 +26,5 @@ int main()
   app->Run();
   delete app;
 }
+
+#endif
